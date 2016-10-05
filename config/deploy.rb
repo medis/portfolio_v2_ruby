a97fa28b2512d47362af61abd20ccf904b237939
Audrius <aubuka@gmail.com>
2016-10-05 21:40:23 +0100

    compile assets locally

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,12 +18,68 @@ set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rben
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
 
+set :keep_assets, 2
+
+Rake::Task["deploy:compile_assets"].clear
+
 after 'deploy:publishing', 'deploy:restart'
 
 namespace :deploy do
   task :restart do
     invoke 'unicorn:reload'
   end
+
+  desc 'Compile assets'
+  task :compile_assets => [:set_rails_env] do
+    # invoke 'deploy:assets:precompile'
+    invoke 'deploy:assets:copy_manifest'
+    invoke 'deploy:assets:precompile_local'
+    invoke 'deploy:assets:backup_manifest'
+  end
+
+  namespace :assets do
+    local_dir = "./public/assets/"
+
+    # Download the asset manifest file so a new one isn't generated. This makes
+    # the app use the latest assets and gives Sprockets a complete manifest so
+    # it knows which files to delete when it cleans up.
+    desc 'Copy assets manifest'
+    task copy_manifest: [:set_rails_env] do
+      on roles(fetch(:assets_roles, [:web])) do
+        remote_dir = "#{fetch(:user)}@#{host.hostname}:#{shared_path}/public/assets/"
+
+        run_locally do
+          begin
+            execute "mkdir #{local_dir}"
+            execute "scp '#{remote_dir}.sprockets-manifest-*' #{local_dir}"
+          rescue
+            # no manifest yet
+          end
+        end
+      end
+    end
+
+    desc "Precompile assets locally and then rsync to web servers"
+    task :precompile_local do
+      # compile assets locally
+      run_locally do
+        execute "RAILS_ENV=#{fetch(:stage)} bundle exec rake assets:precompile"
+      end
+
+      # rsync to each server
+      on roles(fetch(:assets_roles, [:web])) do
+        # this needs to be done outside run_locally in order for host to exist
+        remote_dir = "#{fetch(:user)}@#{host.hostname}:#{shared_path}/public/assets/"
+
+        run_locally do
+          execute "rsync -av #{local_dir} #{remote_dir}"
+        end
+      end
+
+      # clean up
+      run_locally { execute "rm -rf #{local_dir}" }
+    end
+
 end
 
 # Default branch is :master
